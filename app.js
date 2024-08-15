e5f927f8113759f24e4318a92b98a7f9c5714e57
Maneshnarayanan <maneshk27@gmail.com>
2024-08-15 23:06:17 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
-
 const app =express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('views'));
 
 const PORT = process.env.PORT || 3001;
+
+const dbEmail = "mkn@gmail.com"
+const dbPassword = "123"
+
+app.get('/home',(req,res)=>{
+    res.render('home')
+})
+
+app.post('/login',(req,res)=>{ 
+    const {email , password} = req.body
+    if (email===dbEmail && password === dbPassword){
+        res.render('./home')
+    }else {
+        res.status(401).send('Invalid credentials'); 
+    }
+
+
+})
 
 app.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`)
